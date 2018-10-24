c86d36fccc17e52307b94ac06b9f8adf8f3b22a0
Grecko <42585241+supergrecko@users.noreply.github.com>
2018-10-24 18:43:49 +0200

    Update Controller.php

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,18 +5,7 @@ namespace App\Controller;
 class Controller
 {
     /**
-     * Run a view
+     * Parent controller namespace: Add your global controller functions here
      */
-    public function view($name, $data = []) {
-    	extract($data);
-    
-    	$name = str_replace('.', '/', $name) . ".php";
-    		
-    	if (!file_exists("resources/views/$name")) {
-    		throw new ControllerException('No views with this name');
-    	} 
-		
-    	require_once("resources/views/$name");
-    }
     
 }
